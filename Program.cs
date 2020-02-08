b61376d0e1922a4812416fcb11afb3448e35f348
JM [DELL] <jmanuel.rar@emeraldigital.com>
2020-02-08 11:15:39 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace netcore_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
